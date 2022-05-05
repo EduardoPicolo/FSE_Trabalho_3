6126a6ff0233d81a1407e9e0fef1cf75674b7386
igorq937 <igorq937@gmail.com>
2022-05-05 12:50:29 -0300

    Add esp implementação

--- distributed/src/main.c
+++ distributed/src/main.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
+#include "hal/gpio_types.h"
+
+#include "nvs_util.h"
+#include "connection.h"
+#include "dht.h"
+#include "in_device.h"
+#include "out_device.h"
+
+
+#define DHT_PIN         GPIO_NUM_4
+#define OUT_DEVICE_PIN  GPIO_NUM_2
+#define IN_DEVICE_PIN   GPIO_NUM_0
 
 void app_main(void){
-    printf("Hello world!\n");
+
+    nvs_util_setup();
+    connection_setup();
+    dht_setup(DHT_PIN);
+    in_device_setup(IN_DEVICE_PIN);
+    out_device_setup(OUT_DEVICE_PIN);
+
 }
